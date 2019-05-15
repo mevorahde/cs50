a8bfdd7af17608fe04fa82ceb06f45225b0ed40c
David Mevorah <mevorahde@hotmail.com>
2019-05-14 18:13:59 -0700

    Update Project1 to do some refactoring and get started on 'forgot password' functionality.

--- project1/application.py
+++ project1/application.py
@@ -32,7 +32,7 @@ def login():
     username = request.form.get("user_name")
     password = request.form.get("password")
     if db.execute("SELECT user_name, password FROM users WHERE user_name = :username AND password = :password", {"username" :username, "password": password}):
-        return render_template("success.html")
+        return render_template("success.html", message="Welcome back!")
     else:
         return render_template("error.html", message="Invalid Username or Password.")
 
@@ -46,15 +46,28 @@ def register():
 def signup():
     username = request.form.get("username")
     password = request.form.get("password")
+    password_confirm = request.form.get("passwordConfirm")
     email = request.form.get("email")
-    if db.execute("SELECT * FROM users WHERE user_name = :username", {"username": str(username)}).rowcount == 1:
+    if password != password_confirm:
+        return render_template("error.html", message="'Password' and 'Confirm Password' fields do not match. Please have the passwords match.")
+    if db.execute("SELECT user_name, password, email FROM users WHERE user_name = :username", {"username": str(username)}).rowcount == 1:
         return render_template("error.html", message="The username you entered already exists. Please enter in a different username.")
-    if db.execute("SELECT * FROM users WHERE email = :email", {"email": email}).rowcount == 1:
+    if db.execute("SELECT user_name, password, email FROM users WHERE email = :email", {"email": email}).rowcount == 1:
         return render_template("error.html", message="The email you entered already exists. Please enter in a different email.")
     db.execute("INSERT INTO users (user_name, password, email) VALUES (:username, :password, :email)",
         {"username": username, "password": password, "email": email})
     db.commit()
-    return render_template("success.html")
+    return render_template("success.html", message="Registeration complete, thanks for joining!")
+
+
+@app.route("/forgot-password")
+def forgot_password_page():
+    return render_template("forgot-password.html")
+
+
+@app.route("/email-sent")
+def send_password_email():
+    return render_template("forgot-password.html")
 
 
 if __name__ == "__main__":
